d06795349492c67f430b1256130ccd3f16d23eeb
pokurt <amanhmedzahir@hotmail.com>
2020-07-07 07:38:34 +0500

    Chatbot improved
    Instead of enabling chatbot globally now it will enable chatbot per chat_id

--- nana/modules/lydia.py
+++ nana/modules/lydia.py
@@ -1,59 +1,95 @@
 import asyncio
+from time import time
 
 from coffeehouse.api import API
 from coffeehouse.lydia import LydiaAI
+from coffeehouse.exception import CoffeeHouseError as CFError
 from pyrogram import Filters
 
-from nana import lydia_api, app, Command
-from nana.helpers.PyroHelpers import ReplyCheck
-
-lydia_status = False
-coffeehouse_api = None
-lydia = None
-session = None
+from nana import lydia_api, app, Command, setbot, Owner
+import nana.modules.database.lydia_db as sql
 
 __MODULE__ = "Chatbot"
 __HELP__ = """
 An AI Powered Chat Bot Module
 
 ──「 **Chatbot** 」──
--> `lydia`
-Enables AI on replied user & Desables
+-> `addchat`
+Enables AI on chat
+
+-> `rmchat`
+Removes AI on chat
+
 Powered by CoffeeHouse API created by @Intellivoid.
 """
 
-@app.on_message(Filters.me & Filters.command(["lydia"], Command))
-async def lydia_private(_client, message):
-    global lydia_status, coffeehouse_api, lydia, session
-    if lydia_api == "":
-        await message.edit("`lydia API key is not set!\nSet your lydia API key by adding Config Vars in heroku with "
-                           "name lydia_api with value your lydia key API`")
-        return
-    if lydia_status:
-        await message.edit("Turning off lydia...")
-        asyncio.sleep(0.5)
-        lydia_status = False
-        await message.edit("Lydia will not reply your message")
+CoffeeHouseAPI = API(lydia_api)
+api_client = LydiaAI(CoffeeHouseAPI)
+
+
+@app.on_message(Filters.me & Filters.command(["addchat"], Command))
+async def add_chat(_client, message):
+    global api_client
+    chat_id = message.chat.id
+    is_chat = sql.is_chat(chat_id)
+    if not is_chat:
+        ses = api_client.create_session()
+        ses_id = str(ses.id)
+        expires = str(ses.expires)
+        sql.set_ses(chat_id, ses_id, expires)
+        await message.edit("AI successfully enabled for this chat!")
     else:
-        await message.edit("Turning on lydia...")
-        try:
-            coffeehouse_api = API(lydia_api)
-            # Create Lydia instance
-            lydia = LydiaAI(coffeehouse_api)
-            # Create a new chat session (Like a conversation)
-            session = lydia.create_session()
-        except:
-            await message.edit("Wrong lydia API key!")
-            return
-        lydia_status = True
-        await message.edit("now Lydia will reply your message!")
+        await message.edit("AI is already enabled for this chat!")
+
+
+@app.on_message(Filters.me & Filters.command(["rmchat"], Command))
+async def remove_chat(_client, message):
+    chat_id = message.chat.id
+    is_chat = sql.is_chat(chat_id)
+    if not is_chat:
+        await message.edit("AI isn't enabled here in the first place!")
+    else:
+        sql.rem_chat(chat_id)
+        await message.edit("AI disabled successfully!")
 
 
 @app.on_message(~Filters.me & ~Filters.edited & (Filters.mentioned | Filters.private), group=6)
-async def lydia_reply(_client, message):
-    global lydia_status, session
-    if lydia_status:
-        output = session.think_thought(message.text)
-        await message.reply_text(f"`{output}`", quote=True, reply_to_message_id=ReplyCheck(message))
-    else:
+async def chatbot(client, message):
+    global api_client
+    chat_id = message.chat.id
+    is_chat = sql.is_chat(chat_id)
+    if not is_chat:
         return
+    if message.text and not message.document:
+        # if not check_message(client, message):
+        #     return
+        sesh, exp = sql.get_ses(chat_id)
+        query = message.text
+        try:
+            if int(exp) < time():
+                ses = api_client.create_session()
+                ses_id = str(ses.id)
+                expires = str(ses.expires)
+                sql.set_ses(chat_id, ses_id, expires)
+                sesh, exp = sql.get_ses(chat_id)
+        except ValueError:
+            pass
+        try:
+            await client.send_chat_action(chat_id, action='typing')
+            rep = api_client.think_thought(sesh, query)
+            await asyncio.sleep(0.3)
+            await message.reply_text(rep)
+        except CFError as e:
+            await setbot.send_message(
+                Owner, f"Chatbot error: {e} occurred in {chat_id}!")
+
+
+async def check_message(_client, message):
+    reply_msg = await message.reply_to_message
+    if message.text.lower() == "poki":
+        return True
+    if reply_msg:
+        if reply_msg.from_user.id == Owner:
+            return True
+    else:
+        return False
