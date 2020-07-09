acd7edea5df9fef83a9c035ca735d7e400eab002
pokurt <amanhmedzahir@hotmail.com>
2020-07-10 02:59:09 +0500

    added expand url for bitly

--- nana/modules/bitly.py
+++ nana/modules/bitly.py
@@ -1,16 +1,21 @@
 from bitlyshortener import Shortener
 from pyrogram import Filters
+from asyncio import sleep
 
 from nana import app, Command, bitly_token
+from nana.helpers.expand import expand_url
 
-__MODULE__ = "Bitly"
+__MODULE__ = "Link Shortner"
 __HELP__ = """
 This module will shortener your link
 
-──「 **shorten your url** 」──
+──「 **shorten url** 」──
 -> `bitly (link)`
 Shorten your url with bitly
 
+──「 **expand url** 」──
+-> `expand (link)`
+Reply or parse arg of url to expand
 """
 
 
@@ -25,4 +30,32 @@ async def bitly(_client, message):
         await message.edit("Processing")
         urls = [args[1]]
         shortlink = shortener.shorten_urls(urls)
-        await message.edit("Here Your link\n{}".format(shortlink[0]), disable_web_page_preview=True)
+        await message.edit("Here Your link\n{}".format(shortlink[0]),
+                                                       disable_web_page_preview=True
+                                                       )
+
+
+@app.on_message(Filters.command("expand", Command) & Filters.me)
+async def expand(_client, message):
+    if message.reply_to_message:
+        url = message.reply_to_message.text or message.reply_to_message.caption
+    elif len(message.command) > 1:
+        url = message.command[1]
+    else:
+        url = None
+
+    if url:
+        expanded = await expand_url(url)
+        if expanded:
+            await message.edit(
+                f"<b>Shortened URL</b>: {url}\n<b>Expanded URL</b>: {expanded}", disable_web_page_preview=True
+            )
+            return
+        else:
+            await message.edit(
+                "`i Cant expand this url :p`"
+            )
+            await sleep(3)
+            await message.delete()
+    else:
+        await message.edit("Nothing to expand")
