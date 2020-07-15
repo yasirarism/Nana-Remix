845a6a339a2b611b2e4e03f07396e5082caa0871
pokurt <amanhmedzahir@hotmail.com>
2020-07-16 04:29:10 +0500

    few changes to git module

--- nana/modules/github.py
+++ nana/modules/github.py
@@ -25,14 +25,10 @@ This module can help you find information about a github user!
 -> `git (username)`
 Finding information about a github user.
 
-──「 **Github Graph** 」──
--> `ggraph (username)` or `commitgraph (username)`
-Output a user's Commit Graph.
-
 """
 
 @app.on_message(Filters.me & Filters.command("git", Command))
-async def github(_client, message):
+async def github(client, message):
     if len(message.text.split()) == 1:
             await message.edit("Usage: `git (username)`")
             return
@@ -53,51 +49,34 @@ async def github(_client, message):
             created_at = result.get("created_at", "Not Found")
 
             REPLY = (
-                f"**GitHub Info for `{username}**`"
-                f"\n**Username:** `{name}`\n**Bio:** `{bio}`\n**URL:** {url}"
+                f"**GitHub Info for {username}**"
+                f"\n**Username:** `{name}`\n**Bio:** `{bio}`\n**Profile Link:** [Link]({url})"
                 f"\n**Company:** `{company}`\n**Created at:** `{created_at}`"
                 f"\n**Repository:** [Link](https://github.com/{username}?tab=repositories)"
             )
+        url = f"https://ghchart.rshah.org/{username}"
+        file_name = f"{randint(1, 999)}{username}"
+        resp = await AioHttp.get_raw(url)
+        f = await aiofiles.open(f"{file_name}.svg", mode='wb')
+        await f.write(resp)
+        await f.close()
 
-            await message.edit(REPLY)
-
-
-@app.on_message(Filters.command(['ggraph', 'commitgraph'], Command) & Filters.me)
-async def commit_graph(client, message):
-    if len(message.command) < 2:
-        await message.edit("Please provide a github profile username to generate the graph!")
-        await sleep(2)
-        await message.delete()
-        return
-    else:
-        git_user = message.command[1]
-
-    url = f"https://ghchart.rshah.org/{git_user}"
-    file_name = f"{randint(1, 999)}{git_user}"
-
-    resp = await AioHttp.get_raw(url)
-    f = await aiofiles.open(f"{file_name}.svg", mode='wb')
-    await f.write(resp)
-    await f.close()
-
-    try:
-        drawing = svg2rlg(f"{file_name}.svg")
-        renderPM.drawToFile(drawing, f"{file_name}.png")
-    except UnboundLocalError:
-        await message.edit("Username does not exist!")
-        await sleep(2)
-        await message.delete()
-        return
-
-    await asyncio.gather(
-        client.send_photo(
+        try:
+            drawing = svg2rlg(f"{file_name}.svg")
+            renderPM.drawToFile(drawing, f"{file_name}.png")
+        except UnboundLocalError:
+            await message.edit("Username does not exist!")
+            await sleep(2)
+            await message.delete()
+            return
+        await asyncio.gather(
+            client.send_photo(
             chat_id=message.chat.id,
             photo=f"{file_name}.png",
-            caption=git_user,
+            caption=REPLY,
             reply_to_message_id=ReplyCheck(message)
-        ),
+            ),
         message.delete()
     )
-
     for file in iglob(f"{file_name}.*"):
         os.remove(file)
