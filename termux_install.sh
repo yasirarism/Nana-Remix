585d2e809a04c1f73dca0f66e0aab02e9270045d
pokurt <amanhmedzahir@hotmail.com>
2020-07-17 18:38:57 +0500

    termux installation script
    one click dependency install

---
+++ termux_install.sh
@@ -0,0 +1,16 @@
+echo Nana-Remix install for Termux
+echo Written by @pokurt
+echo Beginning dependency installation in 5 seconds....
+sleep 5
+pkg update
+pkg install nano clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
+git clone https://github.com/pokurt/Nana-Remix.git
+cd Nana-Remix
+pip install --upgrade pip setuptools
+pip install -r requirements.txt
+cd nana
+mv config.example.py config.py
+cd ..
+echo Done.
+echo Now edit config.py with nano or anything you want, then run the userbot with py -m nana.
+echo Good luck!
