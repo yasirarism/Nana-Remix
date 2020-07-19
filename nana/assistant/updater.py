f3b481def9e8fbea47076fd4595ecad0d96898d3
pokurt <amanhmedzahir@hotmail.com>
2020-07-19 10:39:15 +0500

    few fixes for assistant updater

--- nana/assistant/updater.py
+++ nana/assistant/updater.py
@@ -3,9 +3,22 @@ import random
 from git import Repo
 from git.exc import GitCommandError, NoSuchPathError, InvalidGitRepositoryError
 from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
-from nana import setbot, Owner, USERBOT_VERSION, ASSISTANT_VERSION, log, OFFICIAL_BRANCH, \
-    REPOSITORY, RANDOM_STICKERS, REMINDER_UPDATE, TEST_DEVELOP, HEROKU_API, DB_AVAILABLE
+from nana import (
+    setbot,
+    Owner,
+    USERBOT_VERSION,
+    ASSISTANT_VERSION,
+    log,
+    OFFICIAL_BRANCH,
+    REPOSITORY,
+    RANDOM_STICKERS,
+    REMINDER_UPDATE,
+    TEST_DEVELOP,
+    HEROKU_API,
+    DB_AVAILABLE,
+)
 from nana.__main__ import restart_all, loop
+
 if DB_AVAILABLE:
     pass
 
@@ -15,7 +28,7 @@ async def gen_chlog(repo, diff):
     d_form = "%H:%M - %d/%m/%y"
     try:
         for cl in repo.iter_commits(diff):
-            changelog += f'• [{cl.committed_datetime.strftime(d_form)}]: {cl.summary} <{cl.author}>\n'
+            changelog += f"• [{cl.committed_datetime.strftime(d_form)}]: {cl.summary} <{cl.author}>\n"
     except GitCommandError:
         changelog = None
     return changelog
@@ -24,7 +37,9 @@ async def gen_chlog(repo, diff):
 async def update_changelog(changelog):
     await setbot.send_sticker(Owner, random.choice(RANDOM_STICKERS))
     text = "**Update successfully!**\n"
-    text += f"🎉 Welcome to Nana Bot v{USERBOT_VERSION} & Assistant v{ASSISTANT_VERSION}\n"
+    text += (
+        f"🎉 Welcome to Nana Bot v{USERBOT_VERSION} & Assistant v{ASSISTANT_VERSION}\n"
+    )
     text += "\n──「 **Update changelogs** 」──\n"
     text += changelog
     await setbot.send_message(Owner, text)
@@ -37,7 +52,9 @@ async def update_checker():
         log.warning(f"Check update failed!\nDirectory {error} is not found!")
         return
     except InvalidGitRepositoryError as error:
-        log.warning(f"Check update failed!\nDirectory {error} does not seems to be a git repository")
+        log.warning(
+            f"Check update failed!\nDirectory {error} does not seems to be a git repository"
+        )
         return
     except GitCommandError as error:
         log.warning(f"Check update failed!\n{error}")
@@ -48,23 +65,25 @@ async def update_checker():
         return
 
     try:
-        repo.create_remote('upstream', REPOSITORY)
+        repo.create_remote("upstream", REPOSITORY)
     except BaseException:
         pass
 
-    upstream = repo.remote('upstream')
+    upstream = repo.remote("upstream")
     upstream.fetch(brname)
-    changelog = await gen_chlog(repo, f'HEAD..upstream/{brname}')
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{brname}")
 
     if not changelog:
-        log.info(f'Nana is up-to-date with branch {brname}')
+        log.info(f"Nana is up-to-date with branch {brname}")
         return
 
-    log.warning(f'New UPDATE available for [{brname}]!')
+    log.warning(f"New UPDATE available for [{brname}]!")
 
     text = f"**New UPDATE available for [{brname}]!**\n\n"
     text += f"**CHANGELOG:**\n`{changelog}`"
-    button = InlineKeyboardMarkup([[InlineKeyboardButton("🔄 Update Now!", callback_data="update_now")]])
+    button = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔄 Update Now!", callback_data="update_now")]]
+    )
     await setbot.send_message(Owner, text, reply_markup=button, parse_mode="markdown")
 
 
@@ -72,20 +91,22 @@ async def update_checker():
 def dynamic_data_filter(data):
     return Filters.create(
         lambda flt, query: flt.data == query.data,
-        data=data  # "data" kwarg is accessed with "flt.data" above
+        data=data,  # "data" kwarg is accessed with "flt.data" above
     )
 
 
 @setbot.on_callback_query(dynamic_data_filter("update_now"))
-async def update_button(_client, query):
-    await query.message.edit_text("Updating, please wait...")
+async def update_button(client, query):
+    await client.send_message(Owner, "Updating, please wait...")
     try:
         repo = Repo()
     except NoSuchPathError as error:
         log.warning(f"Check update failed!\nDirectory {error} is not found!")
         return
     except InvalidGitRepositoryError as error:
-        log.warning(f"Check update failed!\nDirectory {error} does not seems to be a git repository")
+        log.warning(
+            f"Check update failed!\nDirectory {error} does not seems to be a git repository"
+        )
         return
     except GitCommandError as error:
         log.warning(f"Check update failed!\n{error}")
@@ -96,22 +117,22 @@ async def update_button(_client, query):
         return
 
     try:
-        repo.create_remote('upstream', REPOSITORY)
+        repo.create_remote("upstream", REPOSITORY)
     except BaseException:
         pass
 
-    upstream = repo.remote('upstream')
+    upstream = repo.remote("upstream")
     upstream.fetch(brname)
-    changelog = await gen_chlog(repo, f'HEAD..upstream/{brname}')
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{brname}")
     if HEROKU_API is not None:
         import heroku3
+
         heroku = heroku3.from_key(HEROKU_API)
         heroku_applications = heroku.apps()
         if len(heroku_applications) >= 1:
             heroku_app = heroku_applications[0]
             heroku_git_url = heroku_app.git_url.replace(
-                "https://",
-                "https://api:" + HEROKU_API + "@"
+                "https://", "https://api:" + HEROKU_API + "@"
             )
             if "heroku" in repo.remotes:
                 remote = repo.remote("heroku")
@@ -120,18 +141,21 @@ async def update_button(_client, query):
                 remote = repo.create_remote("heroku", heroku_git_url)
             remote.push(refspec="HEAD:refs/heads/master")
         else:
-            await query.reply("no heroku application found, but a key given? 😕 ")
-        await query.reply("Build Unsuccess, Check heroku build log for more detail")
+            await client.send_message(Owner, "no heroku application found, but a key given? 😕 ")
+        await client.send_message(Owner, "Build Unsuccess, Check heroku build log for more detail")
         return
     try:
         upstream.pull(brname)
-        await query.message.edit_text('Successfully Updated!\nBot is restarting...')
+        await client.send_message(Owner, "Successfully Updated!\nBot is restarting...")
     except GitCommandError:
-        repo.git.reset('--hard')
-        repo.git.clean('-fd', 'nana/modules/')
-        repo.git.clean('-fd', 'nana/assistant/')
-        repo.git.clean('-fd', 'nana/helpers/')
-        await query.message.edit_text('Successfully Force Updated!\nBot is restarting...')
+        repo.git.reset("--hard")
+        repo.git.clean("-fd", "nana/modules/")
+        repo.git.clean("-fd", "nana/assistant/")
+        repo.git.clean("-fd", "nana/helpers/")
+        await client.send_message(
+            Owner,
+            "Successfully Force Updated!\nBot is restarting..."
+        )
     await update_changelog(changelog)
     await restart_all()
 
